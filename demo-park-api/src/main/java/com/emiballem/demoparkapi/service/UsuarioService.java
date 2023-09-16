3d80ad2bd182727b0e00d0fd04448c338e3caf15
Gilberto Junior <gilbertopnjunior99@gmail.com>
2023-09-16 15:58:49 -0300

    creating create method

--- demo-park-api/src/main/java/com/emiballem/demoparkapi/service/UsuarioService.java
+++ demo-park-api/src/main/java/com/emiballem/demoparkapi/service/UsuarioService.java
@@ -1,14 +1,20 @@
 package com.emiballem.demoparkapi.service;
 
+import com.emiballem.demoparkapi.entity.Usuario;
 import com.emiballem.demoparkapi.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 @RequiredArgsConstructor
 @Service
 public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
-
+    @Transactional
+    public Usuario salvar(Usuario usuario){
+        return usuarioRepository.save(usuario);
+    }
 
     public UsuarioService(UsuarioRepository usuarioRepository) {
         this.usuarioRepository = usuarioRepository;
--- demo-park-api/src/main/java/com/emiballem/demoparkapi/web/controller/UsuarioController.java
+++ demo-park-api/src/main/java/com/emiballem/demoparkapi/web/controller/UsuarioController.java
@@ -1,7 +1,12 @@
 package com.emiballem.demoparkapi.web.controller;
 
+import com.emiballem.demoparkapi.entity.Usuario;
 import com.emiballem.demoparkapi.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
@@ -11,6 +16,11 @@ public class UsuarioController {
 
     private final UsuarioService usuarioService;
 
+    @PostMapping
+    public ResponseEntity<Usuario> create(@RequestBody Usuario usuario){
+        Usuario user = usuarioService.salvar(usuario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
     public UsuarioController(UsuarioService usuarioService) {
         this.usuarioService = usuarioService;
     }
