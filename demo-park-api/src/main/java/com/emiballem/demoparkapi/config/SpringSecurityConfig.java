b1b377d9b5fc79f2eca12ea675358517a7948ddd
Gilberto Junior <gilbertopnjunior99@gmail.com>
2023-11-06 14:48:06 -0300

    creting jwt token

--- demo-park-api/src/main/java/com/emiballem/demoparkapi/config/SpringSecurityConfig.java
+++ demo-park-api/src/main/java/com/emiballem/demoparkapi/config/SpringSecurityConfig.java
@@ -11,7 +11,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableMethodSecurity
