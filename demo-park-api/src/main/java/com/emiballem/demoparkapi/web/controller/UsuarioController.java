53a738a4c81726d0f2018bd0a65647eb6a2cee66
Gilberto Junior <gilbertopnjunior99@gmail.com>
2023-11-12 17:23:55 -0300

    authorizing client access permission

--- demo-park-api/src/main/java/com/emiballem/demoparkapi/web/controller/UsuarioController.java
+++ demo-park-api/src/main/java/com/emiballem/demoparkapi/web/controller/UsuarioController.java
@@ -53,7 +53,7 @@ public class UsuarioController {
                             content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
             })
     @GetMapping("/{id}")
-    @PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasRole('ADMIN') OR ( hasRole('CLIENTE') AND #id == authentication.principal.id)")
     public ResponseEntity<UsuarioResponseDto> getById(@PathVariable Long id) {
         Usuario user = usuarioService.buscarPorId(id);
         return ResponseEntity.ok(UsuarioMapper.toDto(user));
