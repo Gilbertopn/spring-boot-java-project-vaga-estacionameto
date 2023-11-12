e1c7e9c9a64ca0adc8083aabb9c5f5f6d55c83ba
Gilberto Junior <gilbertopnjunior99@gmail.com>
2023-11-11 21:33:30 -0300

    updating configuration for creating and modifying login

--- demo-park-api/src/main/java/com/emiballem/demoparkapi/entity/Usuario.java
+++ demo-park-api/src/main/java/com/emiballem/demoparkapi/entity/Usuario.java
@@ -4,6 +4,11 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
@@ -12,6 +17,7 @@ import java.util.Objects;
 @Getter @Setter @NoArgsConstructor
 @Entity
 @Table(name = "usuarios")
+@EntityListeners(AuditingEntityListener.class)
 public class Usuario implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,12 +31,19 @@ public class Usuario implements Serializable {
     @Column(name = "role", nullable = false, length = 25)
     private Role role = Role.ROLE_CLIENTE;
 
+    @CreatedDate
     @Column(name = "data_criacao")
     private LocalDateTime dataCriacao;
+
+    @LastModifiedDate
     @Column(name = "data_modificacao")
     private LocalDateTime dataModificacao;
+
+    @CreatedBy
     @Column(name = "criado_por")
     private String criadoPor;
+
+    @LastModifiedBy
     @Column(name = "modificado_por")
     private String modificadoPor;
 
