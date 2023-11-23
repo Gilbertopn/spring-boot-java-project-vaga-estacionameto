9985300ee4de4f9899ec41ab4943c6c15371e0dd
Gilberto Junior <gilbertopnjunior99@gmail.com>
2023-11-23 18:50:58 -0300

    peer-to-peer test for create status 201

--- demo-park-api/src/main/java/com/emiballem/demoparkapi/web/controller/ClienteController.java
+++ demo-park-api/src/main/java/com/emiballem/demoparkapi/web/controller/ClienteController.java
@@ -7,6 +7,12 @@ import com.emiballem.demoparkapi.service.UsuarioService;
 import com.emiballem.demoparkapi.web.dto.ClienteCreateDto;
 import com.emiballem.demoparkapi.web.dto.ClienteResponseDto;
 import com.emiballem.demoparkapi.web.dto.mapper.ClienteMapper;
+import com.emiballem.demoparkapi.web.exception.ErrorMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -26,7 +32,20 @@ public class ClienteController {
     private final ClienteService clienteService;
     private final UsuarioService usuarioService;
 
-
+    @Operation(summary = "Criar um novo cliente",
+            description = "Recurso para criar um novo cliente vinculado a um usuário cadastrado. " +
+                    "Requisição exige uso de um bearer token. Acesso restrito a Role='CLIENTE'",
+            security = @SecurityRequirement(name = "security"),
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Recurso criado com sucesso",
+                            content = @Content(mediaType = " application/json;charset=UTF-8", schema = @Schema(implementation = ClienteResponseDto.class))),
+                    @ApiResponse(responseCode = "409", description = "Cliente CPF já possui cadastro no sistema",
+                            content = @Content(mediaType = " application/json;charset=UTF-8", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "422", description = "Recurso não processado por falta de dados ou dados inválidos",
+                            content = @Content(mediaType = " application/json;charset=UTF-8", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "403", description = "Recurso não permito ao perfil de ADMIN",
+                            content = @Content(mediaType = " application/json;charset=UTF-8", schema = @Schema(implementation = ErrorMessage.class)))
+            })
     @PostMapping
     @PreAuthorize("hasRole('CLIENTE')")
     public ResponseEntity<ClienteResponseDto> create(@RequestBody @Valid ClienteCreateDto dto,
---
+++ demo-park-api/src/test/java/com/emiballem/demoparkapi/ClienteIT.java
@@ -0,0 +1,40 @@
+package com.emiballem.demoparkapi;
+
+import com.emiballem.demoparkapi.web.dto.ClienteCreateDto;
+import com.emiballem.demoparkapi.web.dto.ClienteResponseDto;
+import com.emiballem.demoparkapi.web.exception.ErrorMessage;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(scripts = "/sql/clientes/clientes-insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = "/sql/clientes/clientes-delete.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+public class ClienteIT {
+
+    @Autowired
+    WebTestClient testClient;
+
+    @Test
+    public void criarCliente_ComDadosValidos_RetornarClienteComStatus201() {
+        ClienteResponseDto responseBody = testClient
+                .post()
+                .uri("/api/v1/clientes")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "toby@email.com", "123456"))
+                .bodyValue(new ClienteCreateDto("Tobias Ferreira", "91191064085"))
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(ClienteResponseDto.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getId()).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getNome()).isEqualTo("Tobias Ferreira");
+        org.assertj.core.api.Assertions.assertThat(responseBody.getCpf()).isEqualTo("91191064085");
+    }
+    
+}
