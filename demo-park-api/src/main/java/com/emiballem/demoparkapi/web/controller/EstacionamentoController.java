74e0c2b3fa0b2d613d42a84f22fd91f5dc6429c0
Gilberto Junior <gilbertopnjunior99@gmail.com>
2023-12-10 17:41:32 -0300

    using jasper to create receipt and creating service and controller

--- demo-park-api/src/main/java/com/emiballem/demoparkapi/web/controller/EstacionamentoController.java
+++ demo-park-api/src/main/java/com/emiballem/demoparkapi/web/controller/EstacionamentoController.java
@@ -3,8 +3,10 @@ package com.emiballem.demoparkapi.web.controller;
 import com.emiballem.demoparkapi.entity.ClienteVaga;
 import com.emiballem.demoparkapi.jwt.JwtUserDetails;
 import com.emiballem.demoparkapi.repository.projection.ClienteVagaProjection;
+import com.emiballem.demoparkapi.service.ClienteService;
 import com.emiballem.demoparkapi.service.ClienteVagaService;
 import com.emiballem.demoparkapi.service.EstacionamentoService;
+import com.emiballem.demoparkapi.service.JasperService;
 import com.emiballem.demoparkapi.web.dto.EstacionamentoCreateDto;
 import com.emiballem.demoparkapi.web.dto.EstacionamentoResponseDto;
 import com.emiballem.demoparkapi.web.dto.PageableDto;
@@ -20,6 +22,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -27,12 +30,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.io.IOException;
 import java.net.URI;
 
 import static io.swagger.v3.oas.annotations.enums.ParameterIn.PATH;
@@ -45,6 +50,9 @@ import static io.swagger.v3.oas.annotations.enums.ParameterIn.QUERY;
 public class EstacionamentoController {
     private final EstacionamentoService estacionamentoService;
     private final ClienteVagaService clienteVagaService;
+    private final ClienteService clienteService;
+    private final JasperService jasperService;
+
 
     @Operation(summary = "Operação de check-in", description = "Recurso para dar entrada de um veículo no estacionamento. " +
             "Requisição exige uso de um bearer token. Acesso restrito a Role='ADMIN'",
@@ -186,4 +194,20 @@ public class EstacionamentoController {
         PageableDto dto = PageableMapper.toDto(projection);
         return ResponseEntity.ok(dto);
     }
+
+    @GetMapping("/relatorio")
+    @PreAuthorize("hasRole('CLIENTE')")
+    public ResponseEntity<Void> getRelatorio(HttpServletResponse response, @AuthenticationPrincipal JwtUserDetails user) throws IOException {
+        String cpf = clienteService.buscarPorUsuarioId(user.getId()).getCpf();
+        jasperService.addParams("CPF", cpf);
+
+        byte[] bytes = jasperService.gerarPdf();
+
+        response.setContentType(MediaType.APPLICATION_PDF_VALUE);
+        response.setHeader("Content-disposition", "inline; filename=" + System.currentTimeMillis() + ".pdf");
+        response.getOutputStream().write(bytes);
+
+        return ResponseEntity.ok().build();
+    }
+
 }
